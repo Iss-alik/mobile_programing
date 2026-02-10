64e3d4f98efe4269d4505c06b97380345d4cbc6b
Iss_alik <nyni1305@gmail.com>
2026-02-10 09:49:03 +0500

    Заменить fluttertoast на то что будет работать с виндоус

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alim Lab4',
+      title: 'Alim Lab5',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 62, 183, 58)),
       ),
-      home: const MyHomePage(title: 'Alim Lab4'),
+      home: const MyHomePage(title: 'Alim Lab5'),
     );
   }
 }
@@ -29,94 +30,71 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  late final TabController _tabController;
+
+  @override 
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+
   }
 
- 
+  @override 
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        bottom: TabBar(controller: _tabController, tabs: const <Widget> [Tab(icon: Icon(Icons.list)), Tab(icon: Icon(Icons.grid_view_rounded)), 
+                      Tab(icon: Icon(Icons.card_membership)), Tab(icon: Icon(Icons.draw))   ],),
       ),
 
-      body: Center(
-        child: Stack(
+      body: 
+      TabBarView(
+          controller: _tabController,
           children: [
-            Container(
-                width: 800,
-                height: 800,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  // color: Colors.blue, // for contrast you can add color to container
-                  image: DecorationImage(
-                    image: AssetImage('assets/image1.jpg'),
-                    fit: BoxFit.fill, // .fill - Image fully in the box, no cut only proportion changing.
-                    // .contain - original proportions, no cuts, there are white space
-                    // .cover - Image fully in the box, cutted, proportions saved.
-                    // .fitWidth - fit by the width, can cut by height or free space, try with height - 200 and 700
-                    // .fitHeight -  same to fitWidth but for Height, try with width 200 and 700
-                    // .none - no scaling, can cut, could be wight space
-                    // .scaleDown - deacrease scale if image is not feeting, no cuts, original proportions, could be wight space
-                  ),
-                ),
-            ),
-
-            Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/background.jpeg'),
-                    fit: BoxFit.contain, 
-                  ),
-                ),
-            ),
-
-            Center( 
-              child: Container(
-                width: 400,
-                height: 400,
-                decoration: BoxDecoration(
-                color: const Color.fromARGB(137, 0, 0, 0)
-                ),
-                child: Column(
-                  children:[
-                    Text("Welcome to the flutter", textAlign: TextAlign.center, style: TextStyle(color: const Color.fromARGB(255, 223, 43, 43), fontSize: 35),),
-                    
-                    ElevatedButton(onPressed: (){}, 
-                                  style: ButtonStyle(backgroundColor:  WidgetStatePropertyAll<Color>(Colors.blue), fixedSize: WidgetStateProperty.all<Size>(Size(200, 50)), ), 
-                                  child: Text("Show SnackBar", style: TextStyle(fontSize: 16, color: Colors.white),)),
-
-                    TextButton(onPressed: (){}, 
-                                  style: ButtonStyle(backgroundColor:  WidgetStatePropertyAll<Color>(Colors.transparent), fixedSize: WidgetStateProperty.all<Size>(Size(200, 50)), ), 
-                                  child: Text("Go to second Screen", style: TextStyle(fontSize: 16, color: Colors.green),)),
-
-                    OutlinedButton(onPressed: (){}, 
-                                  style: OutlinedButton.styleFrom( side: BorderSide( color: Colors.black), fixedSize: Size(200, 50)), 
-                                  child: Text("Toggle Image", style: TextStyle(fontSize: 16, color: Colors.black),))
-                  ])
-              ),
-            )
-
-            
-          ],
-        ),
+                  ListView(children: [for(int i =0; i<10; i++) ListTile(title:Text('Item $i'), subtitle: Text('with subtitle $i'), )]), 
+                  GridView.count(crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10, primary: false, padding: EdgeInsets.all(40),
+                                  children: [for(int i=1; i<7; i++) Container(padding: EdgeInsets.all(4), color: Colors.teal[100*i], child: Text('$i'),)],),
+                  Column(mainAxisAlignment: MainAxisAlignment.center, children: [Card(color: Colors.lightGreen[300], child: _SampleCard(cardName: "Elevated Card"),)],),
+                  Text("Hello world")
+                  ]
       ),
 
+    );
+  }
+}
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+class _SampleCard extends StatelessWidget {
+  const _SampleCard({required this.cardName});
+  final String cardName;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(splashColor: Colors.blueGrey, onTap: () {ShowToast("Card $cardName was clicked");}, 
+                  child: SizedBox(width: 200, height: 100, child: Center(child: Text(cardName))),);
+  }
+
+  void ShowToast(String text)
+  {
+    Fluttertoast.showToast(
+        msg: text,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
     );
   }
 }
